5d5909258f5b4e81f570c73a73786c5f24ee0a81
Yesbol <yesbol_gabdullin@hotmail.com>
2021-03-08 20:34:12 +0100

    fix: Fix function name to avoid shadowing

--- technical_indicator.py
+++ technical_indicator.py
@@ -1,10 +1,10 @@
-def sma(df, length=10):
+def simple_ma(df, length=10):
     sma = df['Adj Close'].rolling(window=length).mean()
     return sma
 
 
 def bollinger_bands(df, length=20):
-    mid = sma(df, length=length)
+    mid = simple_ma(df, length=length)
     upper = mid + 2*df['Adj Close'].rolling(window=length).std()
     lower = mid - 2*df['Adj Close'].rolling(window=length).std()
     return mid, upper, lower
