5ba1d9a1ba1f8cdfa172bc09133500abcd8e7ed7
Yesbol <yesbol_gabdullin@hotmail.com>
2021-03-08 20:32:19 +0100

    Add functions sma and bollinger_bands

---
+++ technical_indicator.py
@@ -0,0 +1,11 @@
+def sma(df, length=10):
+    sma = df['Adj Close'].rolling(window=length).mean()
+    return sma
+
+
+def bollinger_bands(df, length=20):
+    mid = sma(df, length=length)
+    upper = mid + 2*df['Adj Close'].rolling(window=length).std()
+    lower = mid - 2*df['Adj Close'].rolling(window=length).std()
+    return mid, upper, lower
+
