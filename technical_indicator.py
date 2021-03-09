626a24d91b6f9bff3a59a99ec6a901282639bc83
Yesbol <yesbol_gabdullin@hotmail.com>
2021-03-09 20:36:14 +0100

    feat: Add functions rate_of_change, price_diff, z_score

--- technical_indicator.py
+++ technical_indicator.py
@@ -3,12 +3,15 @@ import numpy as np
 
 
 def simple_ma(close, length=10):
-    sma = close.rolling(window=length).mean()
-    return sma
+    return close.rolling(window=length).mean()
+
+
+def exp_ma(close, length=10):
+    return close.ewm(span=length).mean()
 
 
 def bollinger_bands(close, length=20):
-    BB_MID = pd.Series(simple_ma(df, length=length), name='BB_MID')
+    BB_MID = pd.Series(simple_ma(close, length=length), name='BB_MID')
     BB_UPPER = pd.Series(BB_MID + 2*close.rolling(window=length).std(), name='BB_UPPER')
     BB_LOWER = pd.Series(BB_MID - 2*close.rolling(window=length).std(), name='BB_LOWER')
     return pd.concat([BB_MID, BB_UPPER, BB_LOWER], axis=1)
@@ -23,3 +26,14 @@ def volume_log(volume):
     return volume.apply(np.log)
 
 
+def rate_of_change(volume):
+    return volume.pct_change()
+
+
+def price_diff(close, periods=1):
+    return close.diff(periods=periods)
+
+
+def z_score(close):
+    ZSCORE = close.apply(lambda x: (x - x.mean() / x.std()))
+    return ZSCORE
