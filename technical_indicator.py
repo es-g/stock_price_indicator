34711f75838d036f3d7bd1d8eedfd51e94d7e8d7
Yesbol <yesbol_gabdullin@hotmail.com>
2021-03-09 20:09:02 +0100

    feat: Add functions bollinger_bands(), daily_return(), volume_log()

--- technical_indicator.py
+++ technical_indicator.py
@@ -1,11 +1,25 @@
-def simple_ma(df, length=10):
-    sma = df['Adj Close'].rolling(window=length).mean()
+import pandas as pd
+import numpy as np
+
+
+def simple_ma(close, length=10):
+    sma = close.rolling(window=length).mean()
     return sma
 
 
-def bollinger_bands(df, length=20):
-    mid = simple_ma(df, length=length)
-    upper = mid + 2*df['Adj Close'].rolling(window=length).std()
-    lower = mid - 2*df['Adj Close'].rolling(window=length).std()
-    return mid, upper, lower
+def bollinger_bands(close, length=20):
+    BB_MID = pd.Series(simple_ma(df, length=length), name='BB_MID')
+    BB_UPPER = pd.Series(BB_MID + 2*close.rolling(window=length).std(), name='BB_UPPER')
+    BB_LOWER = pd.Series(BB_MID - 2*close.rolling(window=length).std(), name='BB_LOWER')
+    return pd.concat([BB_MID, BB_UPPER, BB_LOWER], axis=1)
+
+
+def daily_return(close):
+    daily_r = close.pct_change()
+    return daily_r
+
+
+def volume_log(volume):
+    return volume.apply(np.log)
+
 
